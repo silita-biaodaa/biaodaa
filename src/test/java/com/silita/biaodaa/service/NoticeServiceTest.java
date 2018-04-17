4d805d994c3912bf4f143b7aa7719f15b8f575ef
ddd0401 <ddd0401110@sina.com>
2018-04-17 21:37:57 +0800

    #标大大接口 公告列表修改，详情接口修改，相关公告修改

--- src/test/java/com/silita/biaodaa/service/NoticeServiceTest.java
+++ src/test/java/com/silita/biaodaa/service/NoticeServiceTest.java
@@ -68,8 +68,8 @@ public class NoticeServiceTest extends ConfigTest {
      * @throws Exception
      */
     @Test
-    public void testqueryList()throws Exception{//,"pbModes":"合理定价评审抽取法||综合评估法Ⅰ" ,,"pbModes":"合理定价评审抽取法||综合评估法Ⅰ","projSumStart":"500","projSumEnd":"1000" projSumEnd 2018-04-01  2018-05-30 "pbModes":"合理定价评审抽取法||综合评估法Ⅰ",,"kbDateStart":"2018-01-30","kbDateEnd":"2018-03-13", ,"projSumStart":"100","projSumEnd":"500","zzType":"main_type||4c7d025c-2934-11e5-a311-63b86f04c8dd||4c7d025c-2934-11e5-a311-63b86f04c8dd/3"
-        String requestBody = "{\"pageNo\":1,\"pageSize\":20,\"type\":2,\"projectType\":\"0\",\"projSumStart\":\"500\",\"projSumEnd\":\"1000\",\"pbModes\":\"合理定价评审抽取法||综合评估法Ⅰ\"}";
+    public void testqueryList()throws Exception{//,"projectType":"0","projSumStart":"0","projSumEnd":"1000","regions":"湖南省||长沙市","pbModes":"合理定价评审抽取法||综合评估法Ⅰ" ,,"pbModes":"合理定价评审抽取法||综合评估法Ⅰ","projSumStart":"500","projSumEnd":"1000" projSumEnd 2018-04-01  2018-05-30 "pbModes":"合理定价评审抽取法||综合评估法Ⅰ",,"kbDateStart":"2018-01-30","kbDateEnd":"2018-03-13", ,"projSumStart":"100","projSumEnd":"500","zzType":"main_type||4c7d025c-2934-11e5-a311-63b86f04c8dd||4c7d025c-2934-11e5-a311-63b86f04c8dd/3"
+        String requestBody = "{\"pageNo\":1,\"pageSize\":20,\"type\":1,\"regions\":\"湖南省||长沙市\"}";
         String responseString = mockMvc.perform(post("/notice/queryList").characterEncoding("UTF-8")
                         .contentType(MediaType.APPLICATION_JSON)// contentType(MediaType.APPLICATION_FORM_URLENCODED)//ajax格式 //添加参数(可以添加多个)
                         .content(requestBody.getBytes())//.param("id","3")   //添加参数(可以添加多个)
@@ -83,8 +83,8 @@ public class NoticeServiceTest extends ConfigTest {
 
     @Test   //1805443 1800849 1801046    1805238
     public void testNoticeDetail()throws Exception{
-        String requestBody = "{\"type\":0}";
-        String responseString = mockMvc.perform(post("/notice/detail/1801046").characterEncoding("UTF-8")
+        String requestBody = "{\"type\":2}";
+        String responseString = mockMvc.perform(post("/notice/detail/1805238").characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)// contentType(MediaType.APPLICATION_FORM_URLENCODED)//ajax格式 //添加参数(可以添加多个)
                 .content(requestBody.getBytes())//.param("id","3")   //添加参数(可以添加多个)
         ).andExpect(status().isOk())    //返回的状态是200
