2d8797c33178b89eabb8d26211a3255c693046fb
ddd0401 <ddd0401110@sina.com>
2018-04-25 15:28:49 +0800

    #标大大接口 增加批量清理缓存服务

---
+++ src/main/java/com/silita/biaodaa/controller/RedisController.java
@@ -0,0 +1,43 @@
+package com.silita.biaodaa.controller;
+
+
+import com.silita.biaodaa.common.MyRedisTemplate;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dh on 2017/9/5.
+ */
+@Controller
+public class RedisController {
+    private Log logger = LogFactory.getLog(NoticeController.class);
+
+    @Autowired
+    private MyRedisTemplate myRedisTemplate;
+
+    @RequestMapping(value = "/clearCache/{key}")
+    public String clearCache(Model model, @PathVariable("key") String key, String pwd, String type){
+        try {
+            if (pwd != null && pwd.equals("ybjsadmin")) {
+                if (type != null && type.equals("batch")) {
+                    myRedisTemplate.batchDel(key);
+                    model.addAttribute("info", "[key:" + key + "]批量清理成功。。。");
+                } else {
+                    myRedisTemplate.del(key);
+                    model.addAttribute("info", "[key:" + key + "]单条清理成功");
+                }
+            } else {
+                model.addAttribute("info", "request is failure.");
+            }
+        }catch(Exception e){
+            logger.error(e,e);
+        }
+        return "jsonView";
+    }
+
+}
