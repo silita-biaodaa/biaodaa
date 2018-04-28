0856d73e17e307d7c2e8ba5e4f8c423b7851720a
ddd0401 <ddd0401110@sina.com>
2018-04-28 09:37:16 +0800

    #标大大接口 状态修改为存续

--- src/test/java/com/silita/biaodaa/service/NoticeServiceTest.java
+++ src/test/java/com/silita/biaodaa/service/NoticeServiceTest.java
@@ -99,10 +99,10 @@ public class NoticeServiceTest extends ConfigTest {
         System.out.println("-----返回的json = " + responseString);
     }
 
-    @Test   //1805443 1800849 1801046    1805238
+    @Test   //1805443 1800849 1801046    1805238   多 1813931
     public void testNoticeDetail()throws Exception{
         String requestBody = "{\"type\":0}";
-        String responseString = mockMvc.perform(post("/notice/detail/1800849").characterEncoding("UTF-8")
+        String responseString = mockMvc.perform(post("/notice/detail/1813931").characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)// contentType(MediaType.APPLICATION_FORM_URLENCODED)//ajax格式 //添加参数(可以添加多个)
                 .content(requestBody.getBytes())//.param("id","3")   //添加参数(可以添加多个)
                 .header("X-TOKEN",token)
