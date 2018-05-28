07a78a605b305fe0b78153ced8d41874da94d886
ddd0401 <ddd0401110@sina.com>
2018-05-28 15:18:22 +0800

    #标大大接口

--- src/test/java/com/silita/biaodaa/service/NoticeServiceTest.java
+++ src/test/java/com/silita/biaodaa/service/NoticeServiceTest.java
@@ -73,7 +73,7 @@ public class NoticeServiceTest extends ConfigTest {
      */
     @Test  //,"zzType":"main_type||4c7f85db-2934-11e5-a311-63b86f04c8dd||4c7f85db-2934-11e5-a311-63b86f04c8dd/2"
     public void testqueryList()throws Exception{//,"projectType":"0","regions":"湖南省||长沙市","projectType":"0","title":"小东","projSumStart":"0","projSumEnd":"1000","regions":"湖南省||长沙市","pbModes":"合理定价评审抽取法||综合评估法Ⅰ" ,,"pbModes":"合理定价评审抽取法||综合评估法Ⅰ","projSumStart":"500","projSumEnd":"1000" projSumEnd 2018-04-01  2018-05-30 "pbModes":"合理定价评审抽取法||综合评估法Ⅰ",,"kbDateStart":"2018-01-30","kbDateEnd":"2018-03-13", ,"projSumStart":"100","projSumEnd":"500","zzType":"main_type||4c7d025c-2934-11e5-a311-63b86f04c8dd||4c7d025c-2934-11e5-a311-63b86f04c8dd/3"
-        String requestBody = "{\"com_name\":\"湖南方圆装饰设计工程有限公司\",\"pageNo\":1,\"pageSize\":20,\"type\":2}";
+        String requestBody = "{\"com_name\":\"湖南方圆装饰设计工程有限公司\",\"pageNo\":1,\"pageSize\":20,\"type\":0}";
         String responseString = mockMvc.perform(post("/notice/queryList").characterEncoding("UTF-8")
                         .contentType(MediaType.APPLICATION_JSON)// contentType(MediaType.APPLICATION_FORM_URLENCODED)//ajax格式 //添加参数(可以添加多个)
                         .content(requestBody.getBytes())//.param("id","3")   //添加参数(可以添加多个)
