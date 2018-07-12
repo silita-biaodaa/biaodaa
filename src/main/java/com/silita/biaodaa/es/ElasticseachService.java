168486c817dd9ba5d9f0b7e8e9dc0c7ffd1366a0
zhushuai <cinnabar_s@sina.com>
2018-07-12 18:38:42 +0800

    修改

--- src/main/java/com/silita/biaodaa/es/ElasticseachService.java
+++ src/main/java/com/silita/biaodaa/es/ElasticseachService.java
@@ -3,12 +3,14 @@ package com.silita.biaodaa.es;
 import com.alibaba.fastjson.JSON;
 import com.silita.biaodaa.dao.TbCompanyMapper;
 import com.silita.biaodaa.elastic.common.ConstantUtil;
+import com.silita.biaodaa.elastic.common.NativeElasticSearchUtils;
 import com.silita.biaodaa.elastic.model.PaginationAndSort;
 import com.silita.biaodaa.elastic.model.QuerysModel;
 import com.silita.biaodaa.model.CompanyEs;
-import com.silita.biaodaa.utils.ElasticsearchUtil;
+import com.silita.biaodaa.utils.PropertiesUtils;
 import org.apache.commons.collections.MapUtils;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,16 +25,29 @@ import java.util.Map;
 public class ElasticseachService {
 
 
+    private static String ip = PropertiesUtils.getProperty("ELASTICSEARCH_IP");
+
+    private static String host = PropertiesUtils.getProperty("ELASTICSEARCH_HOST");
+
+    private static String cluster = PropertiesUtils.getProperty("ELASTICSEARCH_CLUSTER");
+
     @Autowired
     TbCompanyMapper tbCompanyMapper;
 
+    @Autowired
+    NativeElasticSearchUtils nativeElasticSearchUtils;
+
+    private TransportClient transportClient;
+
     public void bastchAddCompany() {
+        transportClient = nativeElasticSearchUtils.initClient(ip, cluster, Integer.parseInt(host));
         List<CompanyEs> comList = tbCompanyMapper.queryCompanyEsList();
-        ElasticsearchUtil.nativeElasticSearchUtils.createIndexAndCreateMapping(ElasticsearchUtil.transportClient,CompanyEs.class);
-        ElasticsearchUtil.nativeElasticSearchUtils.batchInsertDate(ElasticsearchUtil.transportClient, "company", "comes", comList);
+        nativeElasticSearchUtils.createIndexAndCreateMapping(transportClient,CompanyEs.class);
+        nativeElasticSearchUtils.batchInsertDate(transportClient, "company", "comes", comList);
     }
 
     public List<Map<String,Object>> quary(Map<String,Object> param) {
+        transportClient = nativeElasticSearchUtils.initClient(ip, cluster, Integer.parseInt(host));
         Map sort = new HashMap<String, String>();
         sort.put("px", SortOrder.DESC);
         String comName = MapUtils.getString(param,"name");
@@ -40,7 +55,7 @@ public class ElasticseachService {
         List<QuerysModel> querys = new ArrayList();
         querys.add(new QuerysModel(ConstantUtil.CONDITION_SHOULD, ConstantUtil.MATCHING_WILDCARD, "comName", comName));
         querys.add(new QuerysModel(ConstantUtil.CONDITION_SHOULD, ConstantUtil.MATCHING_WILDCARD, "comNamePy", comName));
-        SearchResponse response = ElasticsearchUtil.nativeElasticSearchUtils.complexQuery(ElasticsearchUtil.transportClient, "company", "comes", querys, pageSort);
+        SearchResponse response = nativeElasticSearchUtils.complexQuery(transportClient, "company", "comes", querys, pageSort);
         List<Map<String,Object>> list = new ArrayList<>();
         Map<String,Object> map = null;
         for (SearchHit hit : response.getHits()) {
