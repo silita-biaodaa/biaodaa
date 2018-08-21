0dec1677f95f6a05b47de1fa52275289a0a2e502
zhushuai <cinnabar_s@sina.com>
2018-08-21 11:54:27 +0800

    修改企业

--- src/test/java/com/silita/biaodaa/service/CleanEsTest.java
+++ src/test/java/com/silita/biaodaa/service/CleanEsTest.java
@@ -1,10 +1,13 @@
 package com.silita.biaodaa.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.silita.biaodaa.elastic.common.NativeElasticSearchUtils;
 import com.silita.biaodaa.es.InitElasticseach;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
 
 public class CleanEsTest extends ConfigTest{
 
@@ -14,5 +17,28 @@ public class CleanEsTest extends ConfigTest{
     @Test
     public void test(){
         nativeElasticSearchUtils.deleteIndex(InitElasticseach.initClient(),"company");
+    }
+
+    /**
+     * 获取索引doc数量
+     * Elastic Search Java API 2.4
+     */
+    @Test
+    public void getClusterStatus() {
+
+                String response = InitElasticseach.initClient().prepareSearch("company")
+                .setSize(0)
+                .execute()
+                .actionGet()
+                .toString();
+
+        Map<String,Object> jsonObject = (Map<String, Object>) JSON.parse(response);
+        Map<String,Object> jsonMap = (Map<String, Object>) jsonObject.get("hits");
+        String count_num = jsonMap.get("total").toString();
+
+        //System.out.println(JsonResponse.get("hits"));
+
+        System.out.println(count_num);
+
     }
 }
