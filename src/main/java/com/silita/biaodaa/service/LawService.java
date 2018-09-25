d76571fc9fd7e23bf302d85afdae03d0448a6b48
zhushuai <cinnabar_s@sina.com>
2018-09-25 15:36:33 +0800

    修改法务

--- src/main/java/com/silita/biaodaa/service/LawService.java
+++ src/main/java/com/silita/biaodaa/service/LawService.java
@@ -26,10 +26,7 @@ import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class LawService {
@@ -45,6 +42,7 @@ public class LawService {
 
     /**
      * 获取法务列表
+     *
      * @param param
      * @return
      */
@@ -57,22 +55,52 @@ public class LawService {
         String end = MapUtils.getString(param, "end");
         String comName = MapUtils.getString(param, "comName");
         PaginationAndSort pageSort = new PaginationAndSort(MapUtils.getInteger(param, "pageNo"), MapUtils.getInteger(param, "pageSize"), sort);
-        List<QuerysModel> querys = new ArrayList();
-        List<QuerysModel> querys1 = new ArrayList();
+        QueryBuilder queryBuilder1 = null;
+        QueryBuilder queryBuilder2 = null;
+        QueryBuilder queryBuilder3 = null;
+        QueryBuilder queryBuilder4 = null;
+        QueryBuilder queryBuilder5 = null;
+        SearchRequestBuilder requestBuilder = null;
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        BoolQueryBuilder boolQueryBuilder1 = QueryBuilders.boolQuery();
+        BoolQueryBuilder boolQueryBuilder2 = QueryBuilders.boolQuery();
         if (MyStringUtils.isNotNull(comName)) {
-            List<QuerysModel> comQuerys = getCondition(comName, "must");
-            querys.addAll(comQuerys);
+            queryBuilder1 = QueryBuilders.queryStringQuery("\""+comName+"\"").field("content").splitOnWhitespace(false);
+            queryBuilder4 = QueryBuilders.queryStringQuery("\""+comName+"\"").field("title").splitOnWhitespace(false);
+            boolQueryBuilder1.should(queryBuilder1).should(queryBuilder4);
         }
         if (MyStringUtils.isNotNull(keyWord)) {
-            List<QuerysModel> keyQuerys = getCondition(keyWord, "must");
-            querys.addAll(keyQuerys);
+            queryBuilder2 = QueryBuilders.queryStringQuery("\""+keyWord+"\"").field("content").splitOnWhitespace(false);
+            queryBuilder5 = QueryBuilders.queryStringQuery("\""+keyWord+"\"").field("title").splitOnWhitespace(false);
+            boolQueryBuilder2.should(queryBuilder2).should(queryBuilder5);
         }
         if (null != start && null != end) {
             Long[] yearLog = getLong(start, end);
-            querys1.add(new QuerysModel(ConstantUtil.CONDITION_MUST, ConstantUtil.MATCHING_RANGE, "date", yearLog[0] + ConstantUtil.SPLIT_WORDS + yearLog[1]));
-        }
+            queryBuilder3 = QueryBuilders.rangeQuery("date").from(yearLog[0]).to(yearLog[1]).includeLower(true).includeUpper(true);
 
-        SearchResponse response = nativeElasticSearchUtils.complexQuery(client, "biaodaa", "law", querys1, querys, ConstantUtil.CONDITION_MUST, pageSort);
+        }
+        if (null != queryBuilder1 && null != queryBuilder2 && null != queryBuilder3) {
+            boolQueryBuilder.must(boolQueryBuilder1).must(boolQueryBuilder2).must(queryBuilder3);
+            requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(boolQueryBuilder);
+        } else if (null != queryBuilder1 && null != queryBuilder2) {
+            boolQueryBuilder.must(boolQueryBuilder1).must(boolQueryBuilder2);
+            requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(boolQueryBuilder);
+        } else if (null != queryBuilder1 && null != queryBuilder3) {
+            boolQueryBuilder.must(boolQueryBuilder1).must(queryBuilder3);
+            requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(boolQueryBuilder);
+        } else if (null != queryBuilder2 && null != queryBuilder3) {
+            boolQueryBuilder.must(boolQueryBuilder2).must(queryBuilder3);
+            requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(boolQueryBuilder);
+        } else if (null != queryBuilder1) {
+            requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(QueryBuilders.boolQuery().must(queryBuilder1).should(queryBuilder4));
+        } else if (null != queryBuilder2) {
+            requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(QueryBuilders.boolQuery().must(queryBuilder2).should(queryBuilder5));
+        } else if (null != queryBuilder3) {
+            requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(QueryBuilders.boolQuery().must(queryBuilder3));
+        }
+        addSearchBuild(requestBuilder, pageSort);
+        SearchResponse response = requestBuilder.execute().actionGet();
+//        SearchResponse response = nativeElasticSearchUtils.complexQuery(client, "biaodaa", "law", querys1, querys, ConstantUtil.CONDITION_MUST, pageSort);
         if (null == response && response.getHits().getTotalHits() <= 0) {
             return null;
         }
@@ -85,10 +113,10 @@ public class LawService {
         resultMap.put("pages", pages);
         resultMap.put("pageNum", MapUtils.getInteger(param, "pageNo"));
         resultMap.put("pageSize", MapUtils.getInteger(param, "pageSize"));
-        String result = null;
-        JSONObject jsonObject = null;
+        String result;
+        JSONObject jsonObject;
         List<Law> lawList = new ArrayList<>();
-        Law law = null;
+        Law law;
         for (SearchHit hit : response.getHits()) {
             result = hit.getSourceAsString();
             law = new Law();
@@ -110,6 +138,7 @@ public class LawService {
 
     /**
      * 获取法务详情
+     *
      * @param param
      * @return
      */
@@ -140,6 +169,7 @@ public class LawService {
 
     /**
      * 获取时间（Long类型）
+     *
      * @param start
      * @param end
      * @return
@@ -155,24 +185,21 @@ public class LawService {
 
     /**
      * 封装条件
+     *
      * @param condition
      * @param conditionType
      * @return
      */
     private List<QuerysModel> getCondition(String condition, String conditionType) {
         List<QuerysModel> querys = new ArrayList();
-        if ("must".equals(conditionType)) {
-            querys.add(new QuerysModel(ConstantUtil.CONDITION_SHOULD, ConstantUtil.MATCHING_WILDCARD, "content", "*" + condition + "*"));
-            querys.add(new QuerysModel(ConstantUtil.CONDITION_SHOULD, ConstantUtil.MATCHING_WILDCARD, "title", "*" + condition + "*"));
-        } else if ("mustNot".equals(conditionType)) {
-            querys.add(new QuerysModel(ConstantUtil.CONDITION_SHOULD, ConstantUtil.MATCHING_MATCH, "content", "*" + condition + "*"));
-            querys.add(new QuerysModel(ConstantUtil.CONDITION_SHOULD, ConstantUtil.MATCHING_MATCH, "title", "*" + condition + "*"));
-        }
+        querys.add(new QuerysModel(ConstantUtil.CONDITION_SHOULD, ConstantUtil.MATCHING_WILDCARD, "content", "*" + condition + "*"));
+//        querys.add(new QuerysModel(ConstantUtil.CONDITION_SHOULD, ConstantUtil.MATCHING_WILDCARD, "title", "*" + condition + "*"));
         return querys;
     }
 
     /**
      * 获取总数
+     *
      * @param client
      * @param indexName
      * @param type
@@ -208,7 +235,7 @@ public class LawService {
                         companyLawEs = getCompanyLawCount(companyEs.getComName());
                         companyLawEs.setComId(companyEs.getComId());
                         list.add(companyLawEs);
-                        nativeElasticSearchUtils.batchInsertDate(client,"biaodaa", "companyforlaw", list);
+                        nativeElasticSearchUtils.batchInsertDate(client, "biaodaa", "companyforlaw", list);
                     }
                 }
             }
@@ -217,6 +244,7 @@ public class LawService {
 
     /**
      * 查询公司法务
+     *
      * @param comName
      * @return
      */
@@ -225,25 +253,24 @@ public class LawService {
         companyLawEs.setBriCount(0);
         companyLawEs.setComName(comName);
         companyLawEs.setJudCount(0);
-        List<QuerysModel> querys = new ArrayList();
-        querys.add(new QuerysModel(ConstantUtil.CONDITION_MUST, ConstantUtil.MATCHING_WILDCARD, "content", "*" + comName + "*"));
-        querys.add(new QuerysModel(ConstantUtil.CONDITION_MUST, ConstantUtil.MATCHING_WILDCARD, "content", "*行贿*"));
-        SearchResponse response = nativeElasticSearchUtils.complexQuery(client, "biaodaa", "law", new ArrayList<>(), querys, ConstantUtil.CONDITION_MUST, null);
+        QueryBuilder queryBuilder1 = QueryBuilders.queryStringQuery("\""+comName+"\"").field("content").splitOnWhitespace(false);
+        QueryBuilder queryBuilder2 = QueryBuilders.queryStringQuery("\"行贿\"").field("content").splitOnWhitespace(false);
+        SearchRequestBuilder requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(QueryBuilders.boolQuery().must(queryBuilder1).must(queryBuilder2));
+        SearchResponse response = requestBuilder.execute().actionGet();
         Integer briCount = Integer.valueOf(Long.toString(response.getHits().getTotalHits()));
         companyLawEs.setBriCount(briCount);
-        querys = new ArrayList<>();
-        querys.add(new QuerysModel(ConstantUtil.CONDITION_MUST, ConstantUtil.MATCHING_WILDCARD, "content", "*" + comName + "*"));
-        querys.add(new QuerysModel(ConstantUtil.CONDITION_MUST_NOT, ConstantUtil.MATCHING_WILDCARD, "content", "*行贿*"));
-        response = nativeElasticSearchUtils.complexQuery(client, "biaodaa", "law", new ArrayList<>(), querys, ConstantUtil.CONDITION_MUST, null);
+        requestBuilder = client.prepareSearch("biaodaa").setTypes("law").setQuery(QueryBuilders.boolQuery().must(queryBuilder1).mustNot(queryBuilder2));
+        response = requestBuilder.execute().actionGet();
         Integer judCount = Integer.valueOf(Long.toString(response.getHits().getTotalHits()));
         companyLawEs.setJudCount(judCount);
-        companyLawEs.setTotal(companyLawEs.getBriCount()+companyLawEs.getJudCount());
+        companyLawEs.setTotal(companyLawEs.getBriCount() + companyLawEs.getJudCount());
         return companyLawEs;
     }
 
 
     /**
      * 查询公司法务
+     *
      * @param param
      * @return
      */
@@ -253,13 +280,13 @@ public class LawService {
         companyLawEs.setTotal(0);
         companyLawEs.setBriCount(0);
         companyLawEs.setJudCount(0);
-        if(MyStringUtils.isNull(comName)){
+        if (MyStringUtils.isNull(comName)) {
             return companyLawEs;
         }
         List<QuerysModel> querys = new ArrayList();
         querys.add(new QuerysModel(ConstantUtil.CONDITION_MUST, ConstantUtil.MATCHING_TERMS, "comName", comName));
         SearchResponse response = nativeElasticSearchUtils.complexQuery(client, "biaodaa", "companyforlaw", querys, new ArrayList<>(), null, null);
-        if(null == response.getHits() || response.getHits().getTotalHits() <= 0){
+        if (null == response.getHits() || response.getHits().getTotalHits() <= 0) {
             return companyLawEs;
         }
         String result = response.getHits().getAt(0).getSourceAsString();
@@ -274,23 +301,37 @@ public class LawService {
 
     /**
      * 查询公司资质
+     *
      * @param comName
      * @return
      */
-    public String queryCompangScope(String comName){
+    public String queryCompangScope(String comName) {
         String scope = null;
-        if(MyStringUtils.isNull(comName)){
+        if (MyStringUtils.isNull(comName)) {
             return scope;
         }
         List<QuerysModel> querys = new ArrayList();
         querys.add(new QuerysModel(ConstantUtil.CONDITION_MUST, ConstantUtil.MATCHING_TERMS, "comName", comName));
         SearchResponse response = nativeElasticSearchUtils.complexQuery(client, "biaodaa", "company", querys, new ArrayList<>(), null, null);
-        if(null == response.getHits() || response.getHits().getTotalHits() <= 0){
+        if (null == response.getHits() || response.getHits().getTotalHits() <= 0) {
             return scope;
         }
         String result = response.getHits().getAt(0).getSourceAsString();
         JSONObject jsonObject = JSON.parseObject(result);
         scope = jsonObject.getString("operate_range");
         return scope;
+    }
+
+    private void addSearchBuild(SearchRequestBuilder builder, PaginationAndSort paginationAndSort) {
+        if (null != paginationAndSort) {
+            builder = builder.setFrom(paginationAndSort.getStart()).setSize(paginationAndSort.getPageSize());
+            Map<String, SortOrder> sort = paginationAndSort.getSort();
+            Map.Entry entry;
+            if (sort != null && sort.size() > 0) {
+                for (Iterator var19 = sort.entrySet().iterator(); var19.hasNext(); builder = builder.addSort((String) entry.getKey(), (SortOrder) entry.getValue())) {
+                    entry = (Map.Entry) var19.next();
+                }
+            }
+        }
     }
 }
