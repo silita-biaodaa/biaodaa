acfbdaf7201df10a000755de04436b2859fa5cc3
zhushuai <cinnabar_s@sina.com>
2019-09-05 15:07:34 +0800

    #标大大 增加设置订阅接口

--- src/main/java/com/silita/biaodaa/controller/SubscribeController.java
+++ src/main/java/com/silita/biaodaa/controller/SubscribeController.java
@@ -1,7 +1,16 @@
 package com.silita.biaodaa.controller;
 
+import com.silita.biaodaa.common.VisitInfoHolder;
+import com.silita.biaodaa.service.SubscribeService;
+import org.apache.commons.collections.map.HashedMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * 订阅
@@ -11,4 +20,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/subscribe")
 public class SubscribeController extends BaseController{
 
+    @Autowired
+    SubscribeService subscribeService;
+
+    /**
+     * 设置订阅条件
+     *
+     * @param param
+     */
+    @ResponseBody
+    @RequestMapping(value = "/save/condition", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public Map<String, Object> saveCondition(@RequestBody Map<String, Object> param) {
+        Map<String, Object> result = new HashedMap();
+        successMsg(result);
+        subscribeService.saveSubscribeCondition(VisitInfoHolder.getUid(),param);
+        return result;
+    }
+
+    /**
+     * 获取订阅条件
+     *
+     * @param param
+     */
+    @ResponseBody
+    @RequestMapping(value = "/gain/condition", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public Map<String, Object> getCondition(@RequestBody Map<String, Object> param) {
+        Map<String, Object> result = new HashedMap();
+        successMsg(result,subscribeService.getSubscribeCondition(param));
+        return result;
+    }
 }
--- src/main/java/com/silita/biaodaa/service/SubscribeService.java
+++ src/main/java/com/silita/biaodaa/service/SubscribeService.java
@@ -1,6 +1,14 @@
 package com.silita.biaodaa.service;
 
+import com.alibaba.fastjson.JSONObject;
+import com.silita.biaodaa.common.VisitInfoHolder;
+import com.silita.biaodaa.dao.TbUserSubscribeMapper;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.collections.map.HashedMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * 订阅Service
@@ -8,4 +16,49 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SubscribeService {
+
+    @Autowired
+    TbUserSubscribeMapper tbUserSubscribeMapper;
+
+    /**
+     * 设置条件
+     * @param param
+     * @return
+     */
+    public void saveSubscribeCondition(String userId,Map<String,Object> param){
+        //将用户的之前的条件设置为不推送
+        tbUserSubscribeMapper.updateIsPub(userId);
+        if (null == param.get("subType")){
+            param.put("subType","zhaobiao");
+        }
+        Map<String,Object> valMap = new HashedMap(){{
+            put("userId",userId);
+            put("subType",param.get("subType"));
+            put("isPush",param.get("isPush"));
+        }};
+        param.remove("isPush");
+        param.remove("subType");
+        valMap.put("condition", JSONObject.toJSONString(param));
+        tbUserSubscribeMapper.insertUserSubscribe(valMap);
+    }
+
+    /**
+     * 获取用户最新的订阅条件/根据主键查询条件
+     * @param param
+     * @return
+     */
+    public Map<String,Object> getSubscribeCondition(Map<String,Object> param){
+        if (null == param.get("pkid")){
+            param.put("userId", VisitInfoHolder.getUid());
+        }
+        Map<String,Object> result = tbUserSubscribeMapper.queryNewCondition(param);
+        if (MapUtils.isEmpty(result)){
+            return new HashedMap();
+        }
+        Map<String,Object> conditionMap = JSONObject.parseObject(MapUtils.getString(result,"condition"));
+        conditionMap.put("isPush",result.get("isPush"));
+        conditionMap.put("subType",result.get("subType"));
+        conditionMap.put("pkid",result.get("pkid"));
+        return conditionMap;
+    }
 }
