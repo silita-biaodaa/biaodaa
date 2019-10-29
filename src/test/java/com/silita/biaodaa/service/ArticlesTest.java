d6a5f98ed5fe220b5a75173b2b72c71e9a8fa267
zhushuai <cinnabar_s@sina.com>
2019-10-29 11:08:52 +0800

    #标大大  修改行业资讯导出

--- src/test/java/com/silita/biaodaa/service/ArticlesTest.java
+++ src/test/java/com/silita/biaodaa/service/ArticlesTest.java
@@ -7,7 +7,10 @@ import com.mongodb.DBObject;
 import com.silita.biaodaa.dao.ArticlesMapper;
 import com.silita.biaodaa.utils.MongodbUtils;
 import com.silita.biaodaa.utils.PropertiesUtils;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.apache.commons.collections.map.HashedMap;
+import org.codehaus.jackson.map.util.JSONPObject;
+import org.jsoup.Jsoup;
 import org.mongodb.morphia.Datastore;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -26,20 +29,26 @@ public class ArticlesTest extends ConfigTest {
     @org.junit.Test
     public void test() {
         DBCollection dbCollection = db.getDB().getCollection("jst");
-        int total = dbCollection.find(new BasicDBObject("province", "湖南")).count();
-        if (total <= 0) {
-            return;
-        }
+//        int total = dbCollection.find(new BasicDBObject("province", "湖南")).count();
+//        if (total <= 0) {
+//            return;
+//        }
         Map articles;
         DBCursor dbCursor = dbCollection.find(new BasicDBObject("province", "湖南"));
         while (dbCursor.hasNext()) {
             DBObject dbObject = dbCursor.next();
             Map map = dbObject.toMap();
-            articles = new HashedMap(4){{
-                put("time",map.get("date"));
-                put("title",map.get("title"));
-                put("content",map.get("content"));
-                put("url",map.get("url"));
+            articles = new HashedMap(4) {{
+                put("time", map.get("date"));
+                put("title", map.get("title"));
+                put("content", map.get("content"));
+                put("url", map.get("url"));
+                int length = Jsoup.parse(map.get("content").toString()).text().length();
+                if (length > 50) {
+                    put("contentDescribe", Jsoup.parse(map.get("content").toString()).text().substring(0, 50));
+                } else {
+                    put("contentDescribe", Jsoup.parse(map.get("content").toString()).text().substring(0, length));
+                }
             }};
             articlesMapper.insert(articles);
         }
